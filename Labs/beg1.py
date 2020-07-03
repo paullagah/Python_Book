52621a0bb3350a8ddca8938bc2fc07a0d077b3eb
paullagah <plagah@outlook.com>
2020-07-03 09:44:44 +0100

    Beginning lab Exercise 1 complete

---
+++ Labs/beg1.py
@@ -0,0 +1,4 @@
+def output():                           # Defined a function called "output"
+    print("Hello World!")               # Print "Hello World!" to the console
+return output                           # Return the result of the function "output"
+
